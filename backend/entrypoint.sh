be6e426f468f05def4beb73bf0bf8f6a8109dd1b
Hamed <www.hmdsdt@gmail.com>
2025-06-08 15:27:09 -0700

    edited entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 echo "Waiting for MySQL to be ready..."
-while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
-    sleep 2
-done
+sleep 20
 
 echo "Running migrations..."
 python manage.py migrate
