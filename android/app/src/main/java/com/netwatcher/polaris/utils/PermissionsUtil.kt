ae53da6ff526d8310cf4446967f4a319be37be26
Erfan Hemati <efnhmt2020@gmail.com>
2025-06-10 16:03:05 +0330

    Permission Utility Added

---
+++ android/app/src/main/java/com/netwatcher/polaris/utils/PermissionsUtil.kt
@@ -0,0 +1,32 @@
+package eh.learning.homepage.utils
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+object PermissionUtils {
+    private val REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.READ_PHONE_STATE,
+        Manifest.permission.ACCESS_NETWORK_STATE,
+        Manifest.permission.ACCESS_WIFI_STATE,
+        Manifest.permission.INTERNET,
+        Manifest.permission.SEND_SMS
+    )
+
+    fun allPermissionsGranted(context: Context) = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun requestPermissions(activity: Activity, requestCode: Int) {
+        ActivityCompat.requestPermissions(
+            activity,
+            REQUIRED_PERMISSIONS,
+            requestCode
+        )
+    }
+}
