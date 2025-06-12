cc97f51901bc0ee288ceaf3e6334909036bb280c
Hamed <www.hmdsdt@gmail.com>
2025-06-12 16:39:26 -0700

    added CORS

--- backend/polaris/settings.py
+++ backend/polaris/settings.py
@@ -37,12 +37,15 @@ INSTALLED_APPS = [
     # Third party apps
     'rest_framework',
     'drf_yasg',
+    'corsheaders',
     # Local apps
     'users',
     'mobile_reports',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +54,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = '*'
+#CORS_ALLOWED_ORIGINS =[]  #activate this to for deployment and remove CORS_ALLOW_ALL_ORIGINS
 
 ROOT_URLCONF = 'polaris.urls'
 
