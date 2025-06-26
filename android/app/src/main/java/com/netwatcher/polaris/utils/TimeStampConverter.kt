f042158cea41dec79dda80b047cc8fbb52bd8604
Erfan Hemati <efnhmt2020@gmail.com>
2025-06-26 15:23:12 +0330

    Seperation of Concerns for Sending Data to Server

--- android/app/src/main/java/com/netwatcher/polaris/utils/TimeStampConverter.kt
+++ android/app/src/main/java/com/netwatcher/polaris/utils/TimeStampConverter.kt
@@ -3,9 +3,8 @@ package com.netwatcher.polaris.utils
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-fun TimeStampConverter(timestamp: String) : String {
+fun TimeStampConverter(timestamp: String): String {
     val inputFormat = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy")
     val dateTime = LocalDateTime.parse(timestamp, inputFormat)
-    val isoTimestamp = dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "Z"
-    return isoTimestamp
+    return dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + "Z"
 }
