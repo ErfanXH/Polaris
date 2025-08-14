7480536d9df16eb2b3b13aa35fb8adcd3213f42a
Hamed <www.hmdsdt@gmail.com>
2025-08-14 12:16:39 -0700

    all_data hotfix

--- backend/mobile_reports/views.py
+++ backend/mobile_reports/views.py
@@ -50,7 +50,7 @@ class MeasurementViewSet(mixins.CreateModelMixin,
     def get_all(self, request):
         if request.user.is_staff:
             data = Measurement.objects.filter(user__allow_admin_access = True)
-            serializer = self.get_serializer(data=data, many=True)
+            serializer = self.get_serializer(data, many=True)
             serializer.is_valid(raise_exception=True)
         else:
             return Response({'detail': 'access denied:admin access level is required for this operation'},status=status.HTTP_403_FORBIDDEN)
