67bca347bf677d66ea4a1b85aa8c53069e87574a
Hamed <www.hmdsdt@gmail.com>
2025-08-21 14:14:13 +0330

    added new mobile  application

--- frontend/src/pages/Landing/components/VersionInfo.js
+++ frontend/src/pages/Landing/components/VersionInfo.js
@@ -1,7 +1,7 @@
 const versionInfo = {
-    currentVersion: "1.0.0",
+    currentVersion: "1.0.1",
     releaseDate: "August 1, 2025",
-    fileSize: "54.71 MB",
+    fileSize: "56.36 MB",
     requirements: "Android 9.0 - 14.0",
 };
 
